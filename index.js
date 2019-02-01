634cfde86b0f08256d60a09024ecbd64527084c4
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-01 16:01:36 +0530

    db

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 const express = require('express')
 const path = require('path')
+const nforce = require ('nforce')
+var hbs = require('hbs');
 const PORT = process.env.PORT || 5000
+
+function oauthCallbackUrl(req) {
+  return req.protocol + '://' + req.get('host');
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+	  var org = nforce.createConnection({
+      clientId: process.env.CONSUMER_KEY,
+      clientSecret: process.env.CONSUMER_SECRET,
+      redirectUri: oauthCallbackUrl(req),
+      mode: 'single'
+    });
+	
+	if (req.query.code !== undefined) {
+      // authenticated
+      org.authenticate(req.query, function(err) {
+        if (!err) {
+          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {
+            if (!err) {
+              res.render('index', {records: results.records});
+            }
+            else {
+              res.send(err.message);
+            }
+          });
+        }
+        else {
+          if (err.message.indexOf('invalid_grant') >= 0) {
+            res.redirect('/');
+          }
+          else {
+            res.send(err.message);
+          }
+        }
+      });
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
