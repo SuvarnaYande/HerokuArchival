7190bfc932020da45d0907e8f77ee4846d6dd9a0
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-01 20:08:23 +0530

    db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const nforce = require ('nforce')
 var hbs = require('hbs');
-const json2 = require('JSON2/static/json2');
+const JSON = require('JSON2');
 
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
@@ -37,8 +37,8 @@ express()
           org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {		  
             if (!err) {
 			  console.log ("Query result: " + results.records );
-			  console.log ("JSON format: " + json2.Stringify (results.records ));
-			  console.log ("JSON format: " + json2.Parse (results.records ));
+			  console.log ("JSON format: " + JSON.Stringify (results.records ));
+			  console.log ("JSON format: " + JSON.Parse (results.records ));
 			  for (i = 0; i < results.records.length; i++){
 				console.log("single record length: " +  results.records[i].length);
 				for (j = 0; j < results.records[i].length; j++){
