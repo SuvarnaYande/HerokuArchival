e4de20a2377da717237c6cc128a18cd490ec39ff
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-01 20:11:14 +0530

    db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const nforce = require ('nforce')
 var hbs = require('hbs');
-const JSON = require('JSON2');
+const JSON = require('json');
 
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
