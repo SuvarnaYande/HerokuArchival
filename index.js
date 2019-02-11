079cc91a61f81b97eea2d299a7d15c4b895d4c4d
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-11 13:27:40 +0530

    changes

--- index.js
+++ index.js
@@ -32,10 +32,7 @@ express()
             if (!err) {
 				console.log ("Query result: " + results.records );
 				console.log (JSON.stringify (results.records));
-				for (i=0; i<results.records.lenght; i++){
-					
-					console.log (JSON.stringify (results.records[i]));
-				}
+				
               //res.render('index', {records: results.records});
             }
             else {
