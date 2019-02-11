77485d7d0565a921c1cecc9a621cd0111adba1a0
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-11 14:25:56 +0530

    changes

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ express()
 					//console.log (JSON.parse (results.records[i].json));
 				}
 				
-				const client = await pool.connect()
-				await client.query('CREATE TABLE IF NOT EXISTS Account (Id nvarchar (20), name string, type string, industry string, rating string)');
+				const client = pool.connect()
+				client.query('CREATE TABLE IF NOT EXISTS Account (Id nvarchar (20), name string, type string, industry string, rating string)');
 				//await client.query('INSERT INTO Account VALUES ' + JSON.stringify (results.records));
 				//const result = await client.query('SELECT * FROM Account');
 				//const results = { 'results': (result != null) ? result.rows : null};
