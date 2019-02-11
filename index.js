4a206a54bf20ee6e52750f4b36ba17b40edeabdf
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-11 12:15:48 +0530

    changes

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const path = require('path')
+const nforce = require ('nforce')
+var hbs = require('hbs');
+const PORT = process.env.PORT || 5000
+const JSON = require ('JSON2')
+
+function oauthCallbackUrl(req) {
+  return req.protocol + '://' + req.get('host');
+}
+
+hbs.registerHelper('get', function(field) {
+  return this.get(field);
+});
+
+express()
+  .set('view engine', 'hbs')
+  .get('/', (req, res) => {
+	  var org = nforce.createConnection({
+      clientId: process.env.CONSUMER_KEY,
+      clientSecret: process.env.CONSUMER_SECRET,
+      redirectUri: oauthCallbackUrl(req),
+      mode: 'single'
+    });
+	console.log ("req.query.code:: " +req.query.code );
+	if (req.query.code !== undefined) {
+      // authenticated
+	  console.log ("Authentication red code: " + req.query.code );
+      org.authenticate(req.query, function(err) {
+        if (!err) {
+          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {		  
+            if (!err) {
+				console.log ("Query result: " + results.records );
+              res.render('index', {records: results.records});
+            }
+            else {
+              res.send(err.message);
+            }
+          });
+        }
+        else {
+          if (err.message.indexOf('invalid_grant') >= 0) {
+            res.redirect('/');
+          }
+          else {
+            res.send(err.message);
+          }
+        }
+      });
+    }
+	else {
+	  console.log ("Redirect to SFDC" );
+      res.redirect(org.getAuthUri());
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
