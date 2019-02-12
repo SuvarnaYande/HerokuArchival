cc76fa5e5432f8c3213899726df17526d6cd2556
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-12 14:18:36 +0530

    changes

--- index.js
+++ index.js
@@ -85,19 +85,22 @@ express()
 				console.log('INSERT INTO Account VALUES ' + completeResult); 
 				console.log('INSERT INTO Account (' + fieldNames +') VALUES ' + completeResult1); 
 				const client = pool.connect();
-				//pool.query('DROP TABLE IF EXISTS Account');
-				pool.query('CREATE TABLE IF NOT EXISTS Account (name text, type text, industry text, rating text, id text)' , function (err, results, fields){
-					if (!err){
-						pool.query('INSERT INTO Account (' + fieldNames +') VALUES ' + completeResult1);
-						pool.query('SELECT * FROM Account', function (err, rows, fields) {
-							if (!err){
-								console.log ("Data from PG:::::::::::::");
-								//console.log (err);
-								console.log (rows);
-							}
-						});
-					}
+				pool.query('DROP TABLE IF EXISTS Account', function (err, result){
+					pool.query('CREATE TABLE IF NOT EXISTS Account (name text, type text, industry text, rating text, id text)' , function (err, results, fields){
+						if (!err){
+							pool.query('INSERT INTO Account (' + fieldNames +') VALUES ' + completeResult1);
+							pool.query('SELECT * FROM Account', function (err, rows, fields) {
+								if (!err){
+									console.log ("Data from PG:::::::::::::");
+									//console.log (err);
+									console.log (rows);
+								}
+							});
+							pool.end();
+						}
+					});
 				});
+				
 				//pool.query('INSERT INTO Account VALUES ' + completeResult);
 				/*pool.query('INSERT INTO Account VALUES ('{"name":"GenePoint","type":"Customer - Channel","industry":"Biotechnology","rating":"Cold","id":"0017F000007y8FPQAY"},{"name":"United Oil & Gas, UK","type":"Customer - Direct","industry":"Energy","rating":null,"id":"0017F000007y8FNQAY"},{"name":"United Oil & Gas, Singapore","type":"Customer - Direct","industry":"Energy","rating":null,"id":"0017F000007y8FOQAY"},{"name":"Edge Communications","type":"Customer - Direct","industry":"Electronics","rating":"Hot","id":"0017F000007y8FFQAY"},{"name":"Burlington Textiles Corp of America","type":"Customer - Direct","industry":"Apparel","rating":"Warm","id":"0017F000007y8FGQAY"},{"name":"Pyramid Construction Inc.","type":"Customer - Channel","industry":"Construction","rating":null,"id":"0017F000007y8FHQAY"},{"name":"Dickenson plc","type":"Customer - Channel","industry":"Consulting","rating":null,"id":"0017F000007y8FIQAY"},{"name":"Grand Hotels & Resorts Ltd","type":"Customer - Direct","industry":"Hospitality","rating":"Warm","id":"0017F000007y8FJQAY"},{"name":"Express Logistics and Transport","type":"Customer - Channel","industry":"Transportation","rating":"Cold","id":"0017F000007y8FLQAY"},{"name":"University of Arizona","type":"Customer - Direct","industry":"Education","rating":"Warm","id":"0017F000007y8FMQAY"},{"name":"United Oil & Gas Corp.","type":"Customer - Direct","industry":"Energy","rating":"Hot","id":"0017F000007y8FKQAY"},{"name":"sForce","type":null,"industry":null,"rating":null,"id":"0017F000007y8FQQAY"},{"name":"Test 2","type":"Prospect","industry":null,"rating":"Warm","id":"0017F00000ODuA4QAL"},{"name":"Test 2 Lightning Experience","type":null,"industry":null,"rating":null,"id":"0017F00000ODuA9QAL"}'));*/
 				
@@ -111,7 +114,7 @@ express()
 				//console.log ('Data from PG:::: ');
 				//console.log (resultpgs);
 				//res.render('pages/db', results );
-				pool.end();
+				//pool.end();
 	  
                 res.render('index', {records: results.records});
             }
