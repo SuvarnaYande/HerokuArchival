49d3cbe3909694ad3f886ce8ed2ab0c701dfe609
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-12 11:07:09 +0530

    changes

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ express()
         if (!err) {
           org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {		  
             if (!err) {
-				console.log ("Query result: " + results.records );
-				console.log ("Stringify:::"); 
-				console.log (JSON.stringify (results.records));
-				console.log ("Len:: " + JSON.stringify (results.records).length);
+				//console.log ("Query result: " + results.records );
+				//console.log ("Stringify:::"); 
+				//console.log (JSON.stringify (results.records));
+				//console.log ("Len:: " + JSON.stringify (results.records).length);
 				//console.log ("Parse result: " );
 				//console.log (JSON.parse(results.records));
 				
@@ -53,7 +53,7 @@ express()
 				var arrResult = JSON.parse(JSON.stringify (results.records));
 				for (i=0; i<arrResult.length; i++){
 					//console.log (arrResult[i]);
-					console.log (JSON.stringify(arrResult[i]));
+					//console.log (JSON.stringify(arrResult[i]));
 					completeResult += '(\'' + JSON.stringify(arrResult[i]) + '\'),' ;
 					//console.log (arrResult[i].json);
 				}
@@ -68,8 +68,8 @@ express()
 				console.log('INSERT INTO Account VALUES ' + completeResult); 
 				const client = pool.connect();
 				//pool.query('DROP TABLE IF EXISTS Account');
-				pool.query('CREATE TABLE IF NOT EXISTS Account (name text, type text, industry text, rating text, id text)');
-				pool.query('INSERT INTO Account VALUES ' + completeResult);
+				//pool.query('CREATE TABLE IF NOT EXISTS Account (name text, type text, industry text, rating text, id text)');
+				//pool.query('INSERT INTO Account VALUES ' + completeResult);
 				/*pool.query('INSERT INTO Account VALUES ('{"name":"GenePoint","type":"Customer - Channel","industry":"Biotechnology","rating":"Cold","id":"0017F000007y8FPQAY"},{"name":"United Oil & Gas, UK","type":"Customer - Direct","industry":"Energy","rating":null,"id":"0017F000007y8FNQAY"},{"name":"United Oil & Gas, Singapore","type":"Customer - Direct","industry":"Energy","rating":null,"id":"0017F000007y8FOQAY"},{"name":"Edge Communications","type":"Customer - Direct","industry":"Electronics","rating":"Hot","id":"0017F000007y8FFQAY"},{"name":"Burlington Textiles Corp of America","type":"Customer - Direct","industry":"Apparel","rating":"Warm","id":"0017F000007y8FGQAY"},{"name":"Pyramid Construction Inc.","type":"Customer - Channel","industry":"Construction","rating":null,"id":"0017F000007y8FHQAY"},{"name":"Dickenson plc","type":"Customer - Channel","industry":"Consulting","rating":null,"id":"0017F000007y8FIQAY"},{"name":"Grand Hotels & Resorts Ltd","type":"Customer - Direct","industry":"Hospitality","rating":"Warm","id":"0017F000007y8FJQAY"},{"name":"Express Logistics and Transport","type":"Customer - Channel","industry":"Transportation","rating":"Cold","id":"0017F000007y8FLQAY"},{"name":"University of Arizona","type":"Customer - Direct","industry":"Education","rating":"Warm","id":"0017F000007y8FMQAY"},{"name":"United Oil & Gas Corp.","type":"Customer - Direct","industry":"Energy","rating":"Hot","id":"0017F000007y8FKQAY"},{"name":"sForce","type":null,"industry":null,"rating":null,"id":"0017F000007y8FQQAY"},{"name":"Test 2","type":"Prospect","industry":null,"rating":"Warm","id":"0017F00000ODuA4QAL"},{"name":"Test 2 Lightning Experience","type":null,"industry":null,"rating":null,"id":"0017F00000ODuA9QAL"}'));*/
 				
 				const result = pool.query('SELECT * FROM Account');
