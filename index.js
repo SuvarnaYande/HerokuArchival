cde87b6d44271bc01732e646c91eb8a8ec7d2708
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-12 12:34:57 +0530

    changes

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ express()
 	  console.log ("Authentication req code: " + req.query.code );
       org.authenticate(req.query, function(err) {
         if (!err) {
-          org.query({ query: 'SELECT id, name, type, industry, rating FROM Account' }, function(err, results) {		  
+		  var myQuery = 'SELECT id, name, type, industry, rating FROM Account'; 
+          org.query({ query: myQuery }, function(err, results) {		  
             if (!err) {
 				//console.log ("Query result: " + results.records );
 				//console.log ("Stringify:::"); 
@@ -49,14 +50,24 @@ express()
 				//console.log (JSON.parse(JSON.stringify (results.records)));
 				//console.log ("toJSONString result: " );
 				//console.log (JSON.parse(JSON.stringify (results.records)).toJSONString());
+				var fieldNames = myQuery.substring(myQuery.indexOf('SELECT') + 6, myQuery.indexOf('FROM')); 
+				var fields = fieldNames.split(,); 
 				var completeResult = ''; 
+				var completeResult1 = ''; 
 				var arrResult = JSON.parse(JSON.stringify (results.records));
 				for (i=0; i<arrResult.length; i++){
 					//console.log (arrResult[i]);
 					//console.log (JSON.stringify(arrResult[i]));
-					completeResult += '(' + JSON.stringify(arrResult[i]) + '),' ;
+					completeResult += '(\'' + JSON.stringify(arrResult[i]) + '\'),' ;
+					var finalResult = '('; 
+					for (field in fields){
+						finalResult += arrResult[i][field] + ',';
+					}
+					finalResult = finalResult.substring (0, finalResult.length - 1) + ')';
+					completeResult1 += finalResult;
 					//console.log (arrResult[i].json);
 				}
+				
 				/*for (i=0; i<results.records.length; i++){
 					console.log (i);
 					console.log (results.records[i]._fields);
@@ -66,18 +77,18 @@ express()
 				completeResult = completeResult.substring (0, completeResult.length - 1)
 				//console.log (completeResult); 
 				console.log('INSERT INTO Account VALUES ' + completeResult); 
+				console.log('NEWWWWWWWWWWWWWWWWWWW INSERT INTO Account ' + fieldNames +' VALUES ' + completeResult1); 
 				const client = pool.connect();
-				pool.query('DROP TABLE IF EXISTS Account');
-				pool.query('CREATE TABLE IF NOT EXISTS Account (name text, type text, industry text, rating text, id text)');
-				pool.query('INSERT INTO Account VALUES ' + completeResult);
+				//pool.query('DROP TABLE IF EXISTS Account');
+				//pool.query('CREATE TABLE IF NOT EXISTS Account (name text, type text, industry text, rating text, id text)');
+				//pool.query('INSERT INTO Account VALUES ' + completeResult);
 				/*pool.query('INSERT INTO Account VALUES ('{"name":"GenePoint","type":"Customer - Channel","industry":"Biotechnology","rating":"Cold","id":"0017F000007y8FPQAY"},{"name":"United Oil & Gas, UK","type":"Customer - Direct","industry":"Energy","rating":null,"id":"0017F000007y8FNQAY"},{"name":"United Oil & Gas, Singapore","type":"Customer - Direct","industry":"Energy","rating":null,"id":"0017F000007y8FOQAY"},{"name":"Edge Communications","type":"Customer - Direct","industry":"Electronics","rating":"Hot","id":"0017F000007y8FFQAY"},{"name":"Burlington Textiles Corp of America","type":"Customer - Direct","industry":"Apparel","rating":"Warm","id":"0017F000007y8FGQAY"},{"name":"Pyramid Construction Inc.","type":"Customer - Channel","industry":"Construction","rating":null,"id":"0017F000007y8FHQAY"},{"name":"Dickenson plc","type":"Customer - Channel","industry":"Consulting","rating":null,"id":"0017F000007y8FIQAY"},{"name":"Grand Hotels & Resorts Ltd","type":"Customer - Direct","industry":"Hospitality","rating":"Warm","id":"0017F000007y8FJQAY"},{"name":"Express Logistics and Transport","type":"Customer - Channel","industry":"Transportation","rating":"Cold","id":"0017F000007y8FLQAY"},{"name":"University of Arizona","type":"Customer - Direct","industry":"Education","rating":"Warm","id":"0017F000007y8FMQAY"},{"name":"United Oil & Gas Corp.","type":"Customer - Direct","industry":"Energy","rating":"Hot","id":"0017F000007y8FKQAY"},{"name":"sForce","type":null,"industry":null,"rating":null,"id":"0017F000007y8FQQAY"},{"name":"Test 2","type":"Prospect","industry":null,"rating":"Warm","id":"0017F00000ODuA4QAL"},{"name":"Test 2 Lightning Experience","type":null,"industry":null,"rating":null,"id":"0017F00000ODuA9QAL"}'));*/
 				
-				pool.query('SELECT * FROM Account', function (err, rows, fields) {
+				/*pool.query('SELECT * FROM Account', function (err, rows, fields) {
 					console.log ("Data from PG:::::::::::::");
 					console.log (err);
 					console.log (rows);
-					console.log (rows.rows);
-				});
+				});*/
 				
 				//const resultpgs = { 'resultpgs': (result != null) ? result.rows : null};
 				//console.log ('Data from PG:::: ');
