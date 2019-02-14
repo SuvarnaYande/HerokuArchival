078267ac2ac546e195d25d342eab653c9bf0bea0
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-14 11:39:52 +0530

    changes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ hbs.registerHelper('get', function(field) {
 var app = express(); 
 
 app
-  .use(express.bodyParser())
+  .use(express.bodyParser.json())
   .set('view engine', 'hbs')
   .post('/sfdcarchive', (req, res) => {
 	  console.log ('Invoked by SFDC'); 
