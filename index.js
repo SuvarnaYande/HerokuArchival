5c2f642c6a783369d79936897978afd4df6b8413
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-20 20:34:43 +0530

    change

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app
     }
     finalResult = finalResult.substring (0, finalResult.length - 1); 
     console.log('INSERT INTO Account (' + columns.join() +') VALUES ' + finalResult); 
-    
+    console.log('CREATE TABLE IF NOT EXISTS ' + metadata);
   
 	  
 	const client = pool.connect();
