aa8010ce5251442521ae8bb05b505b19bd1887c4
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-02-21 15:53:12 +0530

    changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var app = express();
 app
   .use(bodyParser.json())
   .post('/sfdcarchive', (req, res) => {
-	  console.log ('Invoked by SFDC::::'); 
+	  console.log ('Invoked by SFDC::::::'); 
 	  console.log (req.query.code); 
 	  var reqBody = req.body; 
 	  console.log (reqBody);
