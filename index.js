7f84a79e725e1f593565b122f9cf7222310431bc
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-03-13 14:24:49 +0530

    'ch'

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app
         }
       }
       console.log(fieldArr);
+	  insertFields = insertFields.replace ('Primary Key' , ''); 
 	  insertFieldArr = insertFieldArr.substring (0, insertFieldArr.length - 1);
       upsertFieldArr = upsertFieldArr.substring (0, upsertFieldArr.length - 1)
 	  
