b809a06cde530c31b017f350e2f25197144e78e5
Suvarna Yande <SuvarnaSanket.Yande@cognizant.com>
2019-03-28 11:32:53 +0530

    'ch'

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app
 	  for (i=0; i<records.length; i++){
 		var recordVal = []; 
         for (j =0; j<dataKeyArr.length; j++){
-			var fldVal = records[i][dataKeyArr[j].trim()] ? records[i][dataKeyArr[j].trim()] : NULL;
+			var fldVal = records[i][dataKeyArr[j].trim()] ? records[i][dataKeyArr[j].trim()] : null;
 			/*if (dataTypeArr[j].toUpperCase().trim() == 'BOOLEAN' && fldVal==''){
 				fldVal = false;
 			}*/
